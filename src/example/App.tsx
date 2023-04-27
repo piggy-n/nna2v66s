5489d699724d5710efccb4447a483b1d42f53034
lzn01 <408135242@qq.com>
2023-04-27 13:33:43 +0800

    使用vite-plugin-svg-icons 加载svg图标

--- src/example/App.tsx
+++ src/example/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { Icon } from '../packages/components/Icon';
 
 function App() {
     const [state] = useState(0);
     return (
         <>
             {state}
+            <Icon name={'关闭'} fill={'blue'} />
         </>
     );
 }
---
+++ src/packages/components/Icon/Icon.tsx
@@ -0,0 +1,45 @@
+import 'virtual:svg-icons-register';
+import type { CSSProperties, FC, SVGAttributes } from 'react';
+
+interface IconProps extends SVGAttributes<SVGElement> {
+    name: string;
+    size?: number;
+    title?: string;
+    rotate?: number;
+    useEleStyles?: CSSProperties;
+}
+
+const Icon: FC<IconProps> = (
+    {
+        className,
+        name,
+        fill = '#EFF2F6',
+        size,
+        style,
+        title,
+        rotate,
+        useEleStyles,
+        ...rest
+    },
+) => {
+    return (
+        <svg
+            fill={fill}
+            style={{
+                width: size && `${size}px`,
+                height: size && `${size}px`,
+                transform: `rotate(${rotate ?? 0}deg)`,
+                ...style,
+            }}
+            {...rest}
+        >
+            {
+                title &&
+                <title>{title}</title>
+            }
+            <use xlinkHref={`#ws-${name}`} fill={fill} style={useEleStyles} />
+        </svg>
+    );
+};
+
+export default Icon;
