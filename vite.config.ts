f2059b61a67bdc885220c30627690fa2254f6d04
lzn01 <408135242@qq.com>
2023-05-05 10:38:34 +0800

    删除global.css

--- vite.config.ts
+++ vite.config.ts
@@ -6,42 +6,44 @@ import { defineConfig } from 'vite';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        react(),
-        libCss(),
-        vanillaExtractPlugin(),
-        createSvgIconsPlugin({
-            iconDirs: [path.resolve(process.cwd(), './src/packages/assets/icons')],
-            symbolId: 'ws-[name]',
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(process.cwd(), './src'),
-        },
-    },
-    build: {
-        target: 'es2015',
-        lib: {
-            entry: path.resolve(process.cwd(), './src/packages/index.ts'),
-            name: 'index',
-            fileName: format => `index.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    'react': 'React',
-                    'react-dom': 'ReactDOM',
-                },
+export default defineConfig(
+    ({ command }) => ({
+        plugins: [
+            react(),
+            libCss(),
+            vanillaExtractPlugin(),
+            createSvgIconsPlugin({
+                iconDirs: [path.resolve(process.cwd(), './src/packages/assets/icons')],
+                symbolId: 'ws-[name]',
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(process.cwd(), './src'),
             },
-            plugins: [
-                typescript({
-                    tslib: path.resolve(process.cwd(), './node_modules/typescript'),
-                    outDir: path.resolve(process.cwd(), './dist'),
-                }),
-            ],
         },
-    },
-});
+        build: {
+            target: command === 'serve' ? 'esnext' : 'es2015',
+            lib: {
+                entry: path.resolve(process.cwd(), './src/packages/index.ts'),
+                name: 'index',
+                fileName: format => `index.${format}.js`,
+            },
+            rollupOptions: {
+                external: ['react', 'react-dom'],
+                output: {
+                    globals: {
+                        'react': 'React',
+                        'react-dom': 'ReactDOM',
+                    },
+                },
+                plugins: [
+                    typescript({
+                        tslib: path.resolve(process.cwd(), './node_modules/typescript'),
+                        outDir: path.resolve(process.cwd(), './dist'),
+                    }),
+                ],
+            },
+        },
+    }),
+);
